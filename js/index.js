b25aa131becab3308a7467bd61d60723fdaae0d8
RamiroTrabanco <trabancoramiro@gmail.com>
2022-08-15 13:19:45 -0300

    primer commit

---
+++ js/index.js
@@ -0,0 +1,76 @@
+let verdadero = true
+let falso = false
+let total = 0
+
+const productos = []
+const carrito = []
+
+class Productos {
+    constructor (nombre, valor, stock, id, img){
+        this.nombre=nombre
+        this.valor=valor
+        this.stock=stock
+        this.id=id
+        this.img=img
+    }
+}
+
+productos.push(new Productos("Placa de video Geforce 3050 Palit", 70000, 50, 1432, "imgs/rtx-3050-palit.png"))
+productos.push(new Productos("Placa de video Geforce 3050 Asus", 75000, 50, 1430, "imgs/rtx-3050.png"))
+productos.push(new Productos("Placa de video Geforce 3060 Palit", 90000, 50, 1433, "imgs/rtx-3060-palit.png"))
+productos.push(new Productos("Placa de video Geforce 3070ti GigaByte", 150000, 50, 1600, "imgs/rtx-3070ti-gigabyte.png"))
+productos.push(new Productos("Placa de Video Asrock AMD Radeon RX 6400", 42000, 50, 1603, "https://www.maximus.com.ar/Temp/App_WebSite/App_PictureFiles/Items/90-GA3CZZ-00UANH_800.jpg"))
+productos.push(new Productos("Placa de Video Asrock AMD Radeon RX 6500 XT", 50000, 50, 1606, "https://www.maximus.com.ar/Temp/App_WebSite/App_PictureFiles/Items/90-GA3DZZ-00UANH_800.jpg"))
+productos.push(new Productos("Placa de Video AMD Radeon PowerColor Rx 6600", 77000, 50, 1607, "https://www.maximus.com.ar/Temp/App_WebSite/App_PictureFiles/Items/AXRX-6600-8GBD6-3DH_800.jpg"))
+productos.push(new Productos("Placa de Video AMD Radeon Rx 6650 XT", 95000, 50, 1608, "https://www.maximus.com.ar/Temp/App_WebSite/App_PictureFiles/Items/GV-R665XTGAMING-OC-8GD_800.jpg"))
+
+function agregarProductosAlDom() {
+    let container = document.querySelector(".container");
+    productos.forEach( el => {
+        let div = document.createElement("div");
+        div.className= "card"
+        div.innerHTML = `
+        <img src=${el.img} />
+        <p class="productoTitulo">${el.nombre}</p>
+        <p class="productoPrecio negrita">$${el.valor}</p>
+        <button id="${el.id}" class="botonComprar">Comprar</button>
+        `
+    container.append(div)
+    })
+}
+
+
+/* while (verdadero) {
+    if(confirm("Desea agregar productos al carrito?")) {
+        calcularTotal()
+    } else {
+        break;
+    }
+}
+
+function calcularTotal() {
+    let pedido = prompt("Que producto desea adquirir?")
+
+    const busqueda = productos.find((el) => el.nombre === pedido)
+
+    if (busqueda) {
+        total += busqueda.valor
+    }
+    
+    console.log("El total parcial a pagar es $", total)
+
+    let confirmacion = confirm("Desea agregar más productos al carrito?")
+
+    if (confirmacion) {
+        calcularTotal()
+    } else {
+        console.log("El total a pagar es $", total)
+
+    }
+} */
+
+function buscador() {
+    let buscar = prompt("Ingrese el producto que esta buscando:")
+    const resultadoBusqueda = productos.filter(elemento => elemento.nombre.includes(buscar))
+    console.log(resultadoBusqueda)
+}
